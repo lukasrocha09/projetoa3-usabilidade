c0dcb1f4aaa4e1459a3dd448a824e635e076c1c6
Lukas Rocha <lukas.kh.rocha@gmail.com>
2026-05-21 12:45:47 -0300

    configurando proxy nativo no vite para consumo da api freetogame

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,17 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    proxy: {
+      // Quando o nosso React chamar '/api', o Vite intercepta e manda para o FreeToGame
+      '/api': {
+        target: 'https://www.freetogame.com',
+        changeOrigin: true,
+        secure: false,
+      },
+    },
+  },
 })
